8b08385d4a627383d32dbb886603bff9e5d4a530
Muzamil Khan <113015136+Muzamilofficial@users.noreply.github.com>
2024-12-26 00:38:25 +0500

    configure settings
    add logo to project

--- index.js
+++ index.js
@@ -83,6 +83,7 @@ app.get('/', (req, res) => {
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Correlation and Regression Calculator</title>
+        <link rel="icon" href="images (31).jpeg">
         <style>
             body {
                 font-family: Arial, sans-serif;
